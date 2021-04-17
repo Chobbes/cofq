4b3e73a94423f23d907f4f198d9594e905668e15
Stephen Mell <sm1@cis.upenn.edu>
2021-04-17 11:32:58 -0800

    rename SystemK definitions

--- coq/SystemK/SystemKDefinitions.v
+++ coq/SystemK/SystemKDefinitions.v
@@ -11,20 +11,20 @@ Notation VarInd  := N.
 
 Unset Elimination Schemes.
 Inductive KType : Set :=
-| Prod    : list KType -> KType
-| TForallVecVoid : N -> list KType -> KType
-| TVar    : TypeInd -> KType (* TODO indexing for binders that bind vectors?*)
+| KProd    : list KType -> KType
+| KTForall : N -> list KType -> KType
+| KTVar    : TypeInd -> KType (* TODO indexing for binders that bind vectors?*)
 (* Base Types *)
-| IntType   : KType
+| KIntType   : KType
 .
 Set Elimination Schemes.
 
 Section KTypeInd.
   Variable P : KType -> Prop.
-  Hypothesis IH_IntType         : P IntType.
-  Hypothesis IH_TVar            : forall x, P (TVar x).
-  Hypothesis IH_Prod            : forall (ts: list KType), (forall t, In t ts -> P t) -> P (Prod ts).
-  Hypothesis IH_TForallVecVoid  : forall (n : N) (ts: list KType), (forall t, In t ts -> P t) -> P (TForallVecVoid n ts).
+  Hypothesis IH_IntType         : P KIntType.
+  Hypothesis IH_TVar            : forall x, P (KTVar x).
+  Hypothesis IH_Prod            : forall (ts: list KType), (forall t, In t ts -> P t) -> P (KProd ts).
+  Hypothesis IH_TForall  : forall (n : N) (ts: list KType), (forall t, In t ts -> P t) -> P (KTForall n ts).
 
   Lemma KType_ind : forall (t:KType), P t.
     fix IH 1.
@@ -35,7 +35,7 @@ Section KTypeInd.
         fix IH_ts 1. intros [|u ts']. intros. inversion H.
         intros u' [<-|Hin]. apply IH. eapply IH_ts. apply Hin.
       }
-    - apply IH_TForallVecVoid.
+    - apply IH_TForall.
       { revert l.
         fix IH_ts 1. intros [|u ts']. intros. inversion H.
         intros u' [<-|Hin]. apply IH. eapply IH_ts. apply Hin.
@@ -43,20 +43,20 @@ Section KTypeInd.
   Qed.
 End KTypeInd.
 
-Inductive Value {I} `{FInt I} : Type :=
-| Annotated    : KType -> RawValue -> Value
-with RawValue {I} `{FInt I} : Type :=
-| Num          : I -> RawValue
-| Var          : VarInd -> RawValue
-| Fix          : N -> list KType -> Term -> RawValue
-| Tuple        : list Value -> RawValue 
-with Declaration {I} `{FInt I} : Type :=
-| Val          : Value -> Declaration
-| ProjN        : N -> Value -> Declaration
-| Op           : PrimOp -> Value -> Value -> Declaration  
-with Term {I} `{FInt I} : Type :=
-| Let          : Declaration -> Term -> Term
-| App          : Value -> list KType -> list Value -> Term
-| If0          : Value -> Term -> Term -> Term
-| Halt         : KType -> Value -> Term
+Inductive KValue {I} `{FInt I} : Type :=
+| KAnnotated    : KType -> RawValue -> Value
+with KRawValue {I} `{FInt I} : Type :=
+| KNum          : I -> KRawValue
+| KVar          : VarInd -> KRawValue
+| KFix          : N -> list KType -> KTerm -> KRawValue
+| KTuple        : list KValue -> KRawValue 
+with KDeclaration {I} `{FInt I} : Type :=
+| KVal          : KValue -> KDeclaration
+| KProjN        : N -> KValue -> KDeclaration
+| KOp           : PrimOp -> KValue -> KValue -> KDeclaration  
+with KTerm {I} `{FInt I} : Type :=
+| KLet          : KDeclaration -> KTerm -> KTerm
+| KApp          : KValue -> list KType -> list KValue -> KTerm
+| KIf0          : KValue -> KTerm -> KTerm -> KTerm
+| KHalt         : KType -> KValue -> KTerm
 .
